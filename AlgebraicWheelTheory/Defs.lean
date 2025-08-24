da0fb5f42be6f8faa43612feac7200eda5a7b941
YanYablonovskiy <yablonovskiy.yan@gmail.com>
2025-08-24 12:53:50 +1000

    Changed `InvolutionMonoid` to a class abbrev, define `InvolutionMonoidHom`
    Using `InvolutionMonoid` as abbrev `Monoid` and `StarMul` to engage the mathlib machinery. Defined `InvolutionMonoidHom` . TODO:  Ensure the way `InvolutionMonoidHom` fits in the available (inc mathlib) machinery.

--- AlgebraicWheelTheory/Defs.lean
+++ AlgebraicWheelTheory/Defs.lean
@@ -1,24 +1,50 @@
 import AlgebraicWheelTheory.Basic
 import Mathlib.Logic.Function.Basic
+import Mathlib.Algebra.Star.Basic
 
-class InvolutionMonoid α extends Monoid α where
- op : α → α
- hinv_op (x:α) :  op (op x) = x
 
-namespace InvolutionMonoid
-
-attribute [simp] op
+open Star in
+postfix:100  "⋆" => Star.star
 
 universe u
 
-variable {α : Type u}
+class abbrev InvolutionMonoid (α: Type u) := Monoid α, StarMul α
+
+namespace InvolutionMonoid
+
+universe v
+
+variable {α : Type u} {β : Type v}
+
+instance [DivisionMonoid α] : InvolutionMonoid α where
+ star x := x⁻¹
+ star_involutive := inv_inv
+ star_mul := mul_inv_rev
 
 open Function in
-theorem invol_fun_of_invol_monoid (IM : InvolutionMonoid α) : Involutive (IM.op) := by
- rw [Involutive]; exact (IM.hinv_op : ∀x, op (op x) = x)
+example [InvolutionMonoid α] : Involutive (fun x : α ↦ x⋆) := by
+ simp [Involutive]
 
-instance [InvolutiveInv α] [Monoid α] : InvolutionMonoid α where
- op := fun x ↦ x⁻¹
- hinv_op := fun x ↦ by rw [inv_inv]
+@[ext]
+structure InvolutionMonoidHom (α : Type u) (β : Type v) [InvolutionMonoid α]
+    [InvolutionMonoid β] where
+  toFun : α →ₙ* β
+  invol_hom (x : α) : toFun (x⋆) = (toFun x)⋆
+
+#check IsUnit.star
+
+theorem isUnit_star' [InvolutionMonoid α] {x : α} : IsUnit (x⋆) ↔ IsUnit x := isUnit_star
+
+
+open Function in
+instance [InvolutionMonoid α] [InvolutionMonoid β] : FunLike (InvolutionMonoidHom α β) α β where
+ coe inv_mh := inv_mh.toFun
+ coe_injective' := by
+  rw [Injective]
+  intro x y heq
+  ext z;simp [heq]
+
+variable [Field α]
+
 
 end InvolutionMonoid
